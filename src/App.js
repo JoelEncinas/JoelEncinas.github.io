c4ec524f22253e0bc5a7ca78831891fe344bb40b
JoelEncinas <joelencinasmartin@gmail.com>
2023-02-02 08:56:08 +0100

    added base files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./App.css"
+import About from './components/container/About/About'
+import Contact from './components/container/Contact/Contact'
+import Footer from './components/container/Footer/Footer'
+import Home from './components/container/Home/Home'
+import Portfolio from './components/container/Portfolio/Portfolio'
+import Skills from './components/container/Skills/Skills'
+import Navbar from './components/Navbar/Navbar'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Joel Encinas</p>
-        <p>Web Dev</p>
-      </header>
+    <div>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Portfolio />
+      <Contact />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
